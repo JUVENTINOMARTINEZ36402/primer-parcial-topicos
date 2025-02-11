524e34ef5ac3f39a9e94e3d44eae0a0c6e52e1fa
angelicayazmin <amendoza36671@ucq.edu.mx>
2025-02-10 22:45:44 -0600

    función para ingresar un producto

---
+++ codigo_optimizado.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// Clase Producto
+class Producto {
+private:
+    string nombre;
+    double precio;
+public:
+    // Constructor
+    Producto() {
+        nombre = "";
+        precio = 0.0;
+    }
+    // Setters y Getters
+    void setNombre(string nombre) {
+        this->nombre = nombre;
+    }
+    string getNombre() {
+        return nombre;
+    }
+    void setPrecio(double precio) {
+        this->precio = precio;
+    }
+    double getPrecio() {
+        return precio;
+    }
+};
+
+void ingresarProducto(Producto &producto) {
+    string nombre;
+    double precio;
+    
+    cout << "\nIngresa el nombre del producto: ";
+    cin >> nombre;
+    producto.setNombre(nombre);
+    
+    cout << "Ingresa el precio del producto: ";
+    cin >> precio;
+    producto.setPrecio(precio);
+}
